a4d068f1b1eb4d2b81be20a9b9fd54332df7dedf
jayden000106 <cars0106@icloud.com>
2022-08-30 16:49:11 +0900

    feat: #6 카메라를 위한 ViewController 파일 추가

---
+++ HowExchange/HowExchange/Contoller/CameraViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CameraViewController.swift
+//  HowExchange
+//
+//  Created by 정지혁 on 2022/08/30.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

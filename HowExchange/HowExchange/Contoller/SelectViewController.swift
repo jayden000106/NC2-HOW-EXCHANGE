9e9d8017d733030269452687474a4bf7b11825f1
jayden000106 <cars0106@icloud.com>
2022-08-30 17:38:27 +0900

    feat: 이미지 선택 화면 구현
    - PHPicker를 사용하여 이미지 불러오는 기능 구현
    - 가져온 이미지를 보여줄 Image View 추가

--- HowExchange/HowExchange/Contoller/SelectViewController.swift
+++ HowExchange/HowExchange/Contoller/SelectViewController.swift
@@ -6,11 +6,36 @@
 //
 
 import UIKit
+import PhotosUI
 
 class SelectViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+}
+
+extension SelectViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        for result in results {
+            result.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
+                if let image = object as? UIImage {
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }
+            })
+        }
     }
 }

77ba7de62bf3ddc30910c40630189938abe7a3a3
jayden000106 <cars0106@icloud.com>
2022-08-30 15:02:17 +0900

    feat: #2 화폐 단위 모델 및 열거형 생성
    - 화페 단위를 위한 모델 파일(Unit.swift) 생성
    - enum monetaryUnit 선언, 각 case와 rawValue 생성

---
+++ HowExchange/HowExchange/Model/Unit.swift
@@ -0,0 +1,14 @@
+//
+//  Unit.swift
+//  HowExchange
+//
+//  Created by 정지혁 on 2022/08/30.
+//
+
+import Foundation
+
+enum monetaryUnit: String {
+    case KRW = "KRW"
+    case USD = "USD"
+    case VND = "VND"
+}

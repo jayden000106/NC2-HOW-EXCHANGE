c324ad884a18bce621a122744d108a58c0303db3
jayden000106 <cars0106@icloud.com>
2022-08-30 17:25:40 +0900

    feat: #4 사진 선택 ViewContorller 생성 및 스토리보드 연결

---
+++ HowExchange/HowExchange/Contoller/SelectViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SelectViewController.swift
+//  HowExchange
+//
+//  Created by 정지혁 on 2022/08/30.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
